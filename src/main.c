8a5323b13db793826f6e18119e25ce6edbe4fe01
Ryuichi Tsubuku <rtsubuku@c4r1s1.42tokyo.jp>
2026-02-24 17:07:52 +0900

    update

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rtsubuku <rtsubuku@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/22 14:14:38 by rtsubuku          #+#    #+#             */
-/*   Updated: 2026/02/23 10:24:49 by rtsubuku         ###   ########.fr       */
+/*   Updated: 2026/02/24 15:18:42 by rtsubuku         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,90 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <pthread.h>
+
+typedef struct s_sim
+{
+	long	start_ms;
+}	t_sim;
+
+typedef struct s_coder
+{
+	int		coder_id;
+	t_sim	*sim;
+}	t_coder;
 
 static long	now_ms(void)
 {
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000L + tv.tv_usec / 1000L);
 }
+
+long	elapsed_ms(t_sim *sim)
+{
+	return (now_ms() - sim->start_ms);
+}
+
+void	log_state(t_sim *sim, int coder_id, const char *msg)
+{
+	long	elapsed;
+
+	elapsed = elapsed_ms(sim);
+	printf("%ld %d %s\n", elapsed, coder_id, msg);
+}
+
+void	sleep_ms(long ms)
+{
+	usleep(ms * 1000);
+}
+
+void	*coder_routine(void *arg)
+{
+	int		i;
+	t_coder	*coder;
+
+	i = 0;
+	coder = (t_coder *)arg;
+	while (i < 9)
+	{
+		if (i % 3 == 0)
+			log_state(coder->sim, coder->coder_id, "is compiling");
+		else if (i % 3 == 1)
+			log_state(coder->sim, coder->coder_id, "is debugging");
+		else
+		{
+			log_state(coder->sim, coder->coder_id, "is refactoring");
+		}
+		sleep_ms(100);
+		i++;
+	}
+	return (NULL);
+}
+
+// int	sim_init(t_sim *sim)
+// {
+// 	// 開始時刻・mutex初期化など「会場準備」
+// }
+
+// void	sim_destroy(t_sim *sim)
+// {
+// 	// 後片付け
+// }
 
 int	main(int ac, char **av)
 {
-	int		i;
-	long	start;
-	long	elapsed;
+	t_sim		sim;
+	t_coder		coder;
+	pthread_t	th;
 
 	(void)ac;
 	(void)av;
-	i = 0;
-	start = now_ms();
+	coder.coder_id = 1;
+	coder.sim = &sim;
+	sim.start_ms = now_ms();
+	pthread_create(&th, NULL, coder_routine, &coder);
+	pthread_join(th, NULL);
 	return (0);
 }
